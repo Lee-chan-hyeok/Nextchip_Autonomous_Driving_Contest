f885b04d89543bdd85f9c4b26c0b378694846610
unknown <cksgur8688@naver.com>
2024-11-17 20:57:19 +0900

    Update train_colab_org.py

--- colab/train_colab_org.py
+++ colab/train_colab_org.py
@@ -15,57 +15,22 @@ def result_save_copy(result_dir, name):
     shutil.copy(f'{zip_file}.zip', drive_path)
     print(f'압축 파일이 Google Drive에 저장되었습니다: {drive_path}')
 
+
 # 학습 및 저장 함수
 def train_and_save(name, ep, save_period, batch, result_dir, exist_ok, data):
-    model_path = f'/content/drive/MyDrive/Nextchip_cfg/{name}'
+    model_path = f'/content/files/yaml_files/{name}'
     if not ('.yaml' in name or '.pt' in name):
         print('너 바보, .yaml .pt 둘 다 아님')
         return
 
     model = YOLO(model_path)
-    model.train(data=data, exist_ok=exist_ok, epochs=ep, save_period=save_period, batch=batch, project=result_dir, name=name.split('.')[0])
+    model.train(data=rf'/content/colab/cfg/colab_{data}.yaml', exist_ok=exist_ok, epochs=ep, save_period=save_period, batch=batch, project=result_dir, name=name.split('.')[0])
 
     # 결과를 압축할 폴더 경로 지정
     result_folder = f'{result_dir}/{name.split(".")[0]}'
     result_save_copy(result_folder, name)
 
-# 데이터 압축 해제 함수
-def unzip_data(file_name_list, output_dir_list):
-    for dir in output_dir_list:
-        os.makedirs(dir, exist_ok=True)
-    for idx in range(len(file_name_list)):
-        os.system("unzip " + file_name_list[idx] + " -d " + output_dir_list[idx])
-        
 
-file_name_list = [# nextchip dataset
-                  # images
-                  '/content/drive/MyDrive/Nextchip_dataset/images_train.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/images_valid.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/images_test.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/images_add.zip',
-
-                  # labels
-                  '/content/drive/MyDrive/Nextchip_dataset/labels_train.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/labels_valid.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/labels_test.zip',
-                  '/content/drive/MyDrive/Nextchip_dataset/labels_add.zip',
-
-                  '/content/drive/MyDrive/Nextchip_dataset/path_txt.zip',]
-
-
-output_dir_list = [# nextchip dataset
-                   '/content/Nextchip_dataset/images/train',
-                   '/content/Nextchip_dataset/images/valid',
-                   '/content/Nextchip_dataset/images/test',
-                   '/content/Nextchip_dataset/images/add',
-
-                   '/content/Nextchip_dataset/labels/train',
-                   '/content/Nextchip_dataset/labels/valid',
-                   '/content/Nextchip_dataset/labels/test',
-                   '/content/Nextchip_dataset/labels/add',
-
-
-                   '/content/Nextchip_dataset',]
 
 # Main 함수
 if __name__ == "__main__":
